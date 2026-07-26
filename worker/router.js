686649d1fd861053567c0fc5ff58837dea21d603
vah10.3 <165911392+vah103@users.noreply.github.com>
2026-07-26 22:31:10 +0700

    Use consolidated reminder delivery

--- worker/router.js
+++ worker/router.js
@@ -20,8 +20,8 @@ import {
 import {
   handleReliableReminderRequest,
   isReliableReminderRoute,
-} from "./reliable-reminder-delivery.js";
-import { runNoTopicReminderSchedule } from "./no-topic-reminder-schedule.js";
+  runReliableReminderSchedule,
+} from "./reminder-delivery.js";
 import {
   handleTaskDeleteRequest,
   isTaskDeleteRoute,
@@ -116,6 +116,6 @@ export default {
     });
 
     scheduleIndependentJob(ctx, "weather", () => runRainPushSchedule(env));
-    scheduleIndependentJob(ctx, "reminder", () => runNoTopicReminderSchedule(env));
+    scheduleIndependentJob(ctx, "reminder", () => runReliableReminderSchedule(env));
   },
 };
