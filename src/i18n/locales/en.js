da1d906a3c9087fcab36741b99b6d07c2a21c726
vah10.3 <165911392+vah103@users.noreply.github.com>
2026-08-13 17:37:23 +0700

    Load cleanup copy and sync locale to service worker

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -1,9 +1,12 @@
+import "../sw-locale.js";
 import core from "./en-core.js";
 import hardening from "./en-hardening.js";
+import cleanup from "./en-cleanup.js";
 
 export default Object.freeze({
   ...core,
   ...hardening,
+  ...cleanup,
   "p1008.water": "Household water",
   "p1008.parking": "Parking fee",
 });
