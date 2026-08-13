82a1689a5a8b3ebe9b0cd541bdff8ed0933796b7
vah10.3 <165911392+vah103@users.noreply.github.com>
2026-08-13 17:48:24 +0700

    Load dynamic UI Vietnamese copy

--- src/i18n/locales/vi.js
+++ src/i18n/locales/vi.js
@@ -1,5 +1,6 @@
 import core from "./vi-core.js";
 import hardening from "./vi-hardening.js";
 import cleanup from "./vi-cleanup.js";
+import dynamicUi from "./vi-dynamic-ui.js";
 
-export default Object.freeze({ ...core, ...hardening, ...cleanup });
+export default Object.freeze({ ...core, ...hardening, ...cleanup, ...dynamicUi });
