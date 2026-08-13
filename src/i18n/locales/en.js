fef6868e40f4766eb0f068c62d2f465edad5985c
vah10.3 <165911392+vah103@users.noreply.github.com>
2026-08-13 16:40:00 +0700

    Disambiguate shared water and parking labels

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -1,4 +1,9 @@
 import core from "./en-core.js";
 import hardening from "./en-hardening.js";
 
-export default Object.freeze({ ...core, ...hardening });
+export default Object.freeze({
+  ...core,
+  ...hardening,
+  "p1008.water": "Household water",
+  "p1008.parking": "Parking fee",
+});
