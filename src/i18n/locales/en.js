8e6f4e90aea7ed282ba792d9116c98447b5dfecd
vah10.3 <165911392+vah103@users.noreply.github.com>
2026-08-13 17:48:13 +0700

    Load dynamic UI English copy

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -2,11 +2,13 @@ import "../sw-locale.js";
 import core from "./en-core.js";
 import hardening from "./en-hardening.js";
 import cleanup from "./en-cleanup.js";
+import dynamicUi from "./en-dynamic-ui.js";
 
 export default Object.freeze({
   ...core,
   ...hardening,
   ...cleanup,
+  ...dynamicUi,
   "p1008.water": "Household water",
   "p1008.parking": "Parking fee",
 });
