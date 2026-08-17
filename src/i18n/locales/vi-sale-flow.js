d8edf5cccdca069dffc0d1c6379fbc94653e5198
vah10.3 <165911392+vah103@users.noreply.github.com>
2026-08-17 11:34:23 +0700

    Add Sale close-deal Vietnamese copy

---
+++ src/i18n/locales/vi-sale-flow.js
@@ -0,0 +1,9 @@
+export default Object.freeze({
+  "sales.managerAction": "Quản lý deal",
+  "saleAssistant.closed": "Đã chốt",
+  "saleAssistant.dealSaved": "Đã lưu deal",
+  "saleAssistant.dealSavedHelp": "Deal đã được lưu vào Sale Manager.",
+  "saleAssistant.saveDeal": "Lưu deal",
+  "saleAssistant.savingDeal": "Đang lưu deal…",
+  "saleAssistant.dealSaveFailed": "Chưa lưu được deal. Hãy thử lại.",
+});
