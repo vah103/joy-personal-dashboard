85d7969e5d8d61d0dda8ebb4dec3ff8561ccbafc
vah10.3 <165911392+vah103@users.noreply.github.com>
2026-08-17 11:34:29 +0700

    Register Sale flow English locale

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -4,12 +4,14 @@ import core from "./en-core.js";
 import hardening from "./en-hardening.js";
 import cleanup from "./en-cleanup.js";
 import dynamicUi from "./en-dynamic-ui.js";
+import saleFlow from "./en-sale-flow.js";
 
 export default Object.freeze({
   ...core,
   ...hardening,
   ...cleanup,
   ...dynamicUi,
+  ...saleFlow,
   "p1008.water": "Household water",
   "p1008.parking": "Parking fee",
 });
