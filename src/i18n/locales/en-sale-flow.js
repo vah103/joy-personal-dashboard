b3ef99fdfcbe5cd3df0ba116089d0261e733dffd
vah10.3 <165911392+vah103@users.noreply.github.com>
2026-08-17 11:34:17 +0700

    Add Sale close-deal English copy

---
+++ src/i18n/locales/en-sale-flow.js
@@ -0,0 +1,9 @@
+export default Object.freeze({
+  "sales.managerAction": "Sale Manager",
+  "saleAssistant.closed": "Closed",
+  "saleAssistant.dealSaved": "Deal saved",
+  "saleAssistant.dealSavedHelp": "Deal saved to Sale Manager.",
+  "saleAssistant.saveDeal": "Save deal",
+  "saleAssistant.savingDeal": "Saving deal…",
+  "saleAssistant.dealSaveFailed": "Could not save the deal. Please try again.",
+});
