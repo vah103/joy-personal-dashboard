a0240ca48cb974a736bae5a01e97934077327dba
vah10.3 <165911392+vah103@users.noreply.github.com>
2026-08-17 11:34:36 +0700

    Register Sale flow Vietnamese locale

--- src/i18n/locales/vi.js
+++ src/i18n/locales/vi.js
@@ -2,5 +2,6 @@ import core from "./vi-core.js";
 import hardening from "./vi-hardening.js";
 import cleanup from "./vi-cleanup.js";
 import dynamicUi from "./vi-dynamic-ui.js";
+import saleFlow from "./vi-sale-flow.js";
 
-export default Object.freeze({ ...core, ...hardening, ...cleanup, ...dynamicUi });
+export default Object.freeze({ ...core, ...hardening, ...cleanup, ...dynamicUi, ...saleFlow });
