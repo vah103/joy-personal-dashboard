eec7c9379d6b5338291043a2ea128d6a10a68df9
vah10.3 <165911392+vah103@users.noreply.github.com>
2026-08-26 15:20:26 +0700

    Fold project hub lifecycle cleanup into core

--- src/features/project-hub/project-hub-performance.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  window.addEventListener("pageshow", () => {
-    const modal = document.querySelector("#turtlebot-hub-modal");
-    if (!modal || modal.hidden) document.body.classList.remove("hub-modal-open");
-  });
-})();
